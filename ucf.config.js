557d72bb7f86ffbc54ed4f4684449c0947e34739
Jarvan <liushileijarvan@gmail.com>
2019-09-05 16:10:33 +0800

    d

--- ucf.config.js
+++ ucf.config.js
@@ -13,7 +13,8 @@ module.exports = (env, argv) => {
         // bootList: true,
         // 启动这两个模块，启动调试、构建
         bootList: [
-            "createTable"
+            "createTable",
+            "demonstration",
         ],
         // 代理的配置
         proxy: [
